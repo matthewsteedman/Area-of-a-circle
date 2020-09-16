17e3e4e9b5732744cc447495a953a3afe73f4067
matthewsteedman <msteedman77@gmail.com>
2020-09-16 16:44:34 +0200

    my initial commit

---
+++ Hello.py
@@ -0,0 +1,16 @@
+#print("Hello World! How are you ?")
+#print("Hello!! Im well Thanks and Yourself")
+#print(5+6)
+#firstname=input("what is your name?")
+#print("My name is ", firstname)
+#===============AREA OF A CIRCLE=========================
+
+#from math import pi
+#r = float(input("Radius: "))
+#print("r" + str(1.1) + "is: " + str(pi* 1.1 **2) )
+
+r = int(input("Radius"))
+pi=3.14
+
+circle = (pi *  r * r)
+print (circle)
